3ef9624dac41531b5906ff6c2f770f45baec7d50
Rene Argüelles <rarguelles@cherrera-mac.luxoft.com>
2017-04-07 14:16:37 -0500

    Added OfferRide controller (blank)

---
+++ LuxoftRides/OfferRideViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OfferRideViewController.swift
+//  LuxoftRides
+//
+//  Created by Rene Argüelles on 4/7/17.
+//  Copyright © 2017 Jesus De Meyer. All rights reserved.
+//
+
+import UIKit
+
+class OfferRideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
