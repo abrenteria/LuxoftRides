5a6a7433e1c642acf588d10e377e19d91b2d8292
Rene Argüelles <rarguelles@cherrera-mac.luxoft.com>
2017-04-07 15:37:52 -0500

    Offer ride progress

--- LuxoftRides/OfferRideViewController.swift
+++ LuxoftRides/OfferRideViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import UIKit
+import MapKit
 
 class OfferRideViewController: UIViewController {
+    
+    var originAddress: String {
+        return originTextField.text!
+    }
+    
+    @IBOutlet weak var originTextField: UITextField!
+    @IBOutlet weak var destinationTextField: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let location = "av de los empresarios 135 puerta de hierro jalisco 45030"
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location) { [weak self] placemarks, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                if let placemark = placemarks?.first, let location = placemark.location {
+                    let mark = MKPlacemark(placemark: placemark)
+                    
+                    if var region = self?.mapView.region {
+                        region.center = location.coordinate
+                        region.span.longitudeDelta /= 8.0
+                        region.span.latitudeDelta /= 8.0
+                        self?.mapView.setRegion(region, animated: true)
+                        self?.mapView.addAnnotation(mark)
+                    }
+                }
+            }
+        }
+    }
 
+}
+
+extension OfferRideViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("here")
+        return true
+    }
+    
 }
