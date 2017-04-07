f83530e1d5a3a60cd8bdd0b1c4365dde839cbe90
Jesus De Meyer <jdemeyer@luxoft.com>
2017-04-07 12:28:03 -0500

    ADDED: implemented login functionality to login view controller ADDED: initial view controller that decides whether we need to go to login or to the main screen

---
+++ LuxoftRides/InitialViewController.swift
@@ -0,0 +1,48 @@
+//
+//  InitialViewController.swift
+//  LuxoftRides
+//
+//  Created by Jesus De Meyer on 4/7/17.
+//  Copyright © 2017 Jesus De Meyer. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        indicatorView.startAnimating()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.handleLogin(_:)), userInfo: nil, repeats: false)
+        
+    }
+    
+    func handleLogin(_ sender: Timer) {
+        if let user = LoginManager.shared.loggedInUser {
+            _ = LoginManager.shared.login(email: user.email!, password: user.password!)
+            self.performSegue(withIdentifier: "showMain", sender: nil)
+        } else {
+            self.performSegue(withIdentifier: "showLogin", sender: nil)
+        }
+    }
+
+    // MARK: - Navigation
+ 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        indicatorView.stopAnimating()
+    }
+
+}
---
+++ LuxoftRides/UIViewController+LX.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  LuxoftRides
+//
+//  Created by Jesus De Meyer on 4/7/17.
+//  Copyright © 2017 Jesus De Meyer. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func showSimpleAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
